6baabf37c38b9f06da72905386247f4e55bc2f03
James <jamesjwaggett@gmail.com>
2020-01-25 13:52:49 +0000

    .

--- update.py
+++ update.py
@@ -1,3 +1,4 @@
 import os
 
-os.system("git clone https://github.com/Smooth203/streamingCentre.git");
+os.system("git clone https://github.com/Smooth203/streamingCentre.git C://tmp");
+os.system("move C://tmp C://streamingCentre");
